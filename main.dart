9cfd91de1e2162e32c98936e6450e91ddcd76fd5
bugkingus <bugkingus@gmail.com>
2023-01-19 08:26:11 +0900

    My First Dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+String sayHello({
+  String name = "bug41",
+  String country = "king",
+  int age = 44,
+  }){
+  
+  return ("Hello $name, you are $age, and you come from $country");
+}
+
+void main() {
+  var result = sayHello(
+    name : "",
+    country : "",
+    age : 0
+  );
+  
+  print(result);
+}
